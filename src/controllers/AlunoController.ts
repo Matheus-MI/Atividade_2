d31a0ae74b4dc4a2d3b1c85342dbb3fff12e5f2d
chriss <christiandossantos125@gmail.com>
2025-03-31 08:30:43 -0300

    .

---
+++ src/controllers/AlunoController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express"; 
+import { Aluno } from "../models/Aluno";
+
+export const listarAlunos = async (req: Request, res: Response) => { 
+    const alunos = await Aluno.findAll();
+    return res.json(alunos);
+};
+
+// Cadastrar um novo aluno
+export const cadastrarAluno = async (req: Request, res: Response) => { 
+    const { nome, email, matricula } = req.body;
+
+    let novoAluno = await Aluno.create({ nome, email, matricula });
+    res.status(201).json({
+    message: "Aluno cadastrado com sucesso.",  
+    novoAluno
+    });
+};
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 
-import * as ApiController from '../controllers/apiController';
+import * as AlunoController from '../controllers/AlunoController';
+import * as DisciplinaController from '../controllers/DisciplinaController'
+import * as AlunoDisciplinaController from '../controllers/AlunoDisciplinaController'
 
 const router = Router();
 
-router.get('/ping', ApiController.ping)
+router.get('/listarTodosAlunos', AlunoController.listarAlunos); 
+router.post('/cadastrarAluno', AlunoController.cadastrarAluno);
 
-router
+router.get('/listarTodasDisciplinas', DisciplinaController.listarDisciplinas); 
+router.post('/cadastrarDisciplina', DisciplinaController.cadastrarDisciplina);
 
+router.post('/vincularAlunoADisciplina', AlunoDisciplinaController.vincularAlunoADisciplina); 
+router.get('/listarDisciplinasDoAluno/:alunoId', AlunoDisciplinaController.listarDisciplinasDoAluno);
 
 export default router;
